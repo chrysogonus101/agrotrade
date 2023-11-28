af3f939356ff901bbc79c3a0981d1f1b342b8234
Chrysogonus <Nollybrown0@gmail.com>
2023-11-28 11:26:47 +0100

    modify

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,12 @@
-import Link from "next/Link";
+
+import Link from "next/link";
 import Image from "next/image";
 
 
 export default function Footer () {
   return (
     <footer>
-      <p className="">hello</p>
+      <p className="">hello world</p>
     </footer>
   )
 }
